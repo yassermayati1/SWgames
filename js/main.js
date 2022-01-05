1b5a8bf3761534de8d9be75b0796a951eff6451f
yassermayati1 <96427979+yassermayati1@users.noreply.github.com>
2022-01-05 13:12:36 +0300

    chaqng

--- js/main.js
+++ js/main.js
@@ -159,30 +159,29 @@ event.preventDefault();
 
 /***********************filter************************* */
 $("select").on("click" , function() {
-  
-    $(this).parent(".select-box").toggleClass("open");
-    
-  });
-  
-  $(document).mouseup(function (e)
-  {
-      var container = $(".select-box");
-  
-      if (container.has(e.target).length === 0)
-      {
-          container.removeClass("open");
-      }
-  });
-  
-  
-  $("select").on("change" , function() {
-    
-    var selection = $(this).find("option:selected").text(),
-        labelFor = $(this).attr("id"),
-        label = $("[for='" + labelFor + "']");
-      
-    label.find(".label-desc").html(selection);
-      
-  });
 
- 
+$(this).parent(".select-box").toggleClass("open");
+
+});
+
+$(document).mouseup(function (e)
+{
+    var container = $(".select-box");
+
+    if (container.has(e.target).length === 0)
+    {
+        container.removeClass("open");
+    }
+});
+
+
+$("select").on("change" , function() {
+
+var selection = $(this).find("option:selected").text(),
+    labelFor = $(this).attr("id"),
+    label = $("[for='" + labelFor + "']");
+    
+label.find(".label-desc").html(selection);
+    
+});
+/***************multi**************************** */
